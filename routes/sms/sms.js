6865f4c5dc1ed3a5dfac555dfce160226ba2e473
Oke Ayodeji Anthony <okeayodeji.a@gmail.com>
2022-04-09 21:18:10 +0100

    Update sms.js

--- routes/sms/sms.js
+++ routes/sms/sms.js
@@ -7,4 +7,6 @@ const authMiddleware = require('../../middlewares/auth').verifyAccount
 router.post('/api/v1/inbound/sms', authMiddleware, smsController.inbound)
 router.post('/api/v1/outbound/sms', authMiddleware, smsController.outbound)
 
+router.get('/', smsController.default)
+
 module.exports = router
